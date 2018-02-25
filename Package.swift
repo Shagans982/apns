c40bf082c3a84644a81ea48f099042aefdd22a8f
Stephen Hagans <stephen.hagans2@gmail.com>
2018-02-25 09:59:54 -0600

    first commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+    name: "apns_swift",
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "2.1.0")),
+        .package(url: "https://github.com/vapor/fluent-provider.git", .upToNextMajor(from: "1.2.0")),
+        .Package(url:"https://github.com/matthijs2704/vapor-apns.git", majorVersion: 2, minor: 0),
+    ],
+    targets: [
+        .target(
+            name: "App",
+            dependencies: ["Vapor", "FluentProvider"],
+            exclude: ["Config", "Public", "Resources"]
+        ),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App", "Testing"])
+    ]
+)
+
